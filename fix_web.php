8269c0ea69c146f0f4400b5e69e0f1b9fb62970f
irhamkaraman <irhamkaraman@gmail.com>
2026-09-01 11:54:38 +0700

    chore: cleanup temporary scripts and test files

--- fix_web.php
+++
@@ -1,6 +0,0 @@
-<?php
-$c = file_get_contents('routes/web.php');
-$c = str_replace([' Illuminate\Http', ' Spatie\Permission', ' App\Http'], [' \Illuminate\Http', ' \Spatie\Permission', ' \App\Http'], $c);
-$c = str_replace(['[App\Http', '[Spatie\Permission', ' Illuminate\Support'], ['[\App\Http', '[\Spatie\Permission', ' \Illuminate\Support'], $c);
-file_put_contents('routes/web.php', $c);
-echo "Fixed slashes in web.php";
--- fix_web2.php
+++
@@ -1,6 +0,0 @@
-<?php
-$content = file_get_contents('routes/web.php');
-// Add leading slash to Illuminate, App, Spatie if not already there, not in a "use" statement, and not part of another word
-$content = preg_replace('/(?<!\\\\)(?<!use\s)(?<!\w)(Illuminate|App|Spatie)\b/', '\\\$1', $content);
-file_put_contents('routes/web.php', $content);
-echo "Regex replaced.\n";
