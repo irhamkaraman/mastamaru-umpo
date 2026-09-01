f69b6980f4825c2edece6384a166de4ef9d9531c
irhamkaraman <irhamkaraman@gmail.com>
2026-09-01 18:38:11 +0700

    Remove redundant umpo:sync-mahasiswa closure command

--- routes/console.php
+++ routes/console.php
@@ -1,12 +1,8 @@
 <?php
-
-use App\Console\Commands\SyncUmpoMahasiswa;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
-Artisan::command('umpo:sync-mahasiswa', function () {
-    $this->call(SyncUmpoMahasiswa::class);
-})->purpose('Sync active students from UMPO API for year 2026');
+
